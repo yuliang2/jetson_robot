b588585ed874417b645af029a3e5880b03a905b7
zuozuojia <2674928186@qq.com>
2024-10-28 22:48:57 +0800

    update motor pos control, test ok

--- python/a1_motor_control.py
+++ python/a1_motor_control.py
@@ -19,8 +19,11 @@ class A1_Motor:
         motor_mode (A1_Params.ModeType): 0 -- stop, 10 -- FOC control. When the value of `motor_mode` is 0, \
             the motor enters brake mode. At this point the other control parameters have no effect \
             and the motor will stop rotating. 
+        reduction_ratio (float): reduction ratio of motor, for A1 always be 9.1.
     """
-    def __init__(self, serial_id, motor_id, mode):
+    def __init__(self, serial_id, motor_id, mode, reduction_ratio=9.1, 
+                 max_angle=None, min_angle=None, max_speed=None, min_speed=None, 
+                 max_tau=None, min_tau=None):
         assert serial_id in [0, 1, 2, 3], "serial_id should be 0, 1, 2 or 3"
         assert motor_id in [0, 1, 2], "motor_id should be 0, 1 or 2"
         assert mode in [0, 10], "mode should be 0-stop or 10-FOC"
@@ -28,11 +31,56 @@ class A1_Motor:
         self.motor_id = motor_id
         self.serial_name = f'/dev/ttyUSB{serial_id}'
         self.serial = SerialPort(self.serial_name)
+        self.reduction_ratio = reduction_ratio
+
+        if max_angle == None:
+            self.max_angle = 823549 / self.reduction_ratio
+        else:
+            assert max_angle <= 823549 / self.reduction_ratio, "max_angle should less or equal to 823549 / reduction_ratio"
+            assert max_angle >= - 823549 / self.reduction_ratio, "max_angle should more or equal to - 823549 / reduction_ratio"
+            self.max_angle = max_angle
+
+        if min_angle == None:
+            self.min_angle = - 823549 / self.reduction_ratio
+        else:
+            assert min_angle <= 823549 / self.reduction_ratio, "min_angle should less or equal to 823549 / reduction_ratio"
+            assert min_angle >= - 823549 / self.reduction_ratio, "min_angle should more or equal to - 823549 / reduction_ratio"
+            self.min_angle = min_angle
+
+        if max_speed == None:
+            self.max_speed = 256 / self.reduction_ratio
+        else:
+            assert max_speed <= 256 / self.reduction_ratio, "max_speed should less or equal to 256 / reduction_ratio"
+            assert max_speed >= - 256 / self.reduction_ratio, "max_speed should more or equal to - 256 / reduction_ratio"
+            self.max_speed = max_speed
+
+        if min_speed == None:
+            self.min_speed = - 256 / self.reduction_ratio
+        else:
+            assert min_speed <= 256 / self.reduction_ratio, "min_speed should less or equal to 256 / reduction_ratio"
+            assert min_speed >= - 256 / self.reduction_ratio, "min_speed should more or equal to - 256 / reduction_ratio"
+            self.min_speed = min_speed
+
+        if max_tau == None:
+            self.max_tau = 128
+        else:
+            assert max_tau <= 128, "max_tau should less or equal to 128"
+            assert max_tau >= - 128, "max_tau should more or equal to - 128"
+            self.max_tau = max_tau
+
+        if min_tau == None:
+            self.min_tau = -128
+        else:
+            assert min_tau <= 128, "min_tau should less or equal to 128"
+            assert min_tau >= - 128, "min_tau should more or equal to - 128"
+            self.min_tau = min_tau
+
         self.cmd = MotorCmd()
         self.data = MotorData()
 
         self.data.motorType = MotorType.A1
         self.cmd.motorType = MotorType.A1
+
         self.cmd.mode = mode
         self.cmd.id   = 0
         self.cmd.q    = 0.0
@@ -41,21 +89,78 @@ class A1_Motor:
         self.cmd.kd   = 0
         self.cmd.tau  = 0.0
     
+    def SetParams(self, kp, kd):
+        r"""
+        Refresh kp and kd.
+
+        Args:
+            kp (float, 0-16): proportional gain.
+            kd (float, 0-32): derivative gain.
+        Return:
+            True: Set success.
+            False: Set failed, kp or kd exceeded limits.
+        """
+        if (kp >= 0 and kp <= 16) and (kd >= 0 and kd <= 32):
+            self.cmd.kp = kp
+            self.cmd.kd = kd
+            return True
+        else:
+            return False
+    
+    def ReadData(self):
+        self.serial.sendRecv(self.cmd, self.data)
+        return self.data
+
+    def SetLimitTau(self, max_tau, min_tau):
+        assert max_tau <= 823549 / self.reduction_ratio, "max_tau should less or equal to 823549 / reduction_ratio"
+        assert max_tau >= - 823549 / self.reduction_ratio, "max_tau should more or equal to - 823549 / reduction_ratio"
+        assert min_tau <= 823549 / self.reduction_ratio, "min_tau should less or equal to 823549 / reduction_ratio"
+        assert min_tau >= - 823549 / self.reduction_ratio, "min_tau should more or equal to - 823549 / reduction_ratio"
+
+        self.max_tau = max_tau
+        self.min_tau = min_tau
+    
     def SetLimitPos(self, max_angle, min_angle):
-        assert max_angle <= 1024, "max_angle should less or equal to 1024"
-        assert min_angle >= 0, "max_angle should more or equal to 0"
+        assert max_angle <= 823549 / self.reduction_ratio, "max_angle should less or equal to 823549 / reduction_ratio"
+        assert max_angle >= - 823549 / self.reduction_ratio, "max_angle should more or equal to - 823549 / reduction_ratio"
+        assert min_angle <= 823549 / self.reduction_ratio, "min_angle should less or equal to 823549 / reduction_ratio"
+        assert min_angle >= - 823549 / self.reduction_ratio, "min_angle should more or equal to - 823549 / reduction_ratio"
+
         self.max_angle = max_angle
         self.min_angle = min_angle
     
     def SetLimitSpeed(self, max_speed, min_speed):
-        assert max_speed <= 1024, "max_speed should less or equal to 1024"
-        assert min_speed >= 0, "min_speed should more or equal to 0"
+        assert max_speed <= 256 / self.reduction_ratio, "max_speed should less or equal to 256 / reduction_ratio"
+        assert max_speed >= - 256 / self.reduction_ratio, "max_speed should more or equal to - 256 / reduction_ratio"
+        assert min_speed <= 256 / self.reduction_ratio, "min_speed should less or equal to 256 / reduction_ratio"
+        assert min_speed >= - 256 / self.reduction_ratio, "min_speed should more or equal to - 256 / reduction_ratio"
+
         self.max_speed = max_speed
         self.min_speed = min_speed
 
-    def PosControl(self, pos):
+    def AbsPosControl(self, tau, abs_pos):
         r"""
-        Cotrol Motor by position. And can refresh the latest motor status.
+        Cotrol Motor by absolute position. And can refresh the latest motor status.
+
+        Args:
+            abs_pos (float, 0-1024): aim position.
+        Return:
+            True: Set success.
+            False: Set failed, target position exceeded limits.
+        """
+        # TODO: other params, auto control?
+        if abs_pos > self.max_angle or abs_pos < self.min_angle:
+            return False
+        self.cmd.mode = 10  # FOC
+        self.cmd.q    = abs_pos * self.reduction_ratio
+        self.cmd.dq   = 0.0
+        self.cmd.tau  = tau
+        self.serial.sendRecv(self.cmd, self.data)
+        return True
+
+    def IncPosControl(self, tau, inc_pos):
+        r"""
+        Cotrol Motor by incremental position. And can refresh the latest motor status.
 
         Args:
             pos (float, 0-1024): aim position.
@@ -64,19 +169,63 @@ class A1_Motor:
             False: Set failed, target position exceeded limits.
         """
         # TODO: other params, auto control?
-        if pos > self.max_angle or pos < self.min_angle:
+        # self.serial.sendRecv(self.cmd, self.data)
+        abs_pos = self.data.q/self.reduction_ratio
+        aim_abs_pos = abs_pos + inc_pos
+        if aim_abs_pos > self.max_angle or aim_abs_pos < self.min_angle:
             return False
         self.cmd.mode = 10  # FOC
-        self.cmd.q = pos
+        self.cmd.q    = aim_abs_pos * self.reduction_ratio
+        self.cmd.dq   = 0.0
+        self.cmd.tau  = tau
         self.serial.sendRecv(self.cmd, self.data)
+        return True
 
 
+# params = A1_Params()
+# motor1 = A1_Motor(serial_id=0, motor_id=0, 
+#                   mode=params.ModeType.STOP, reduction_ratio=9.2)
 
+# time.sleep(0.5)
+
+# motor1.SetParams(0.2, 1.0)
+
+# motor1.AbsPosControl(0, 0)
+# time.sleep(1)
+# motor1.ReadData()
+# print('position: ', motor1.data.q/motor1.reduction_ratio)
+
+# motor1.AbsPosControl(0, 1.57)
+# time.sleep(1)
+# motor1.ReadData()
+# print('position: ', motor1.data.q/motor1.reduction_ratio)
+
+# motor1.AbsPosControl(0, 3.14)
+# time.sleep(1)
+# motor1.ReadData()
+# print('position: ', motor1.data.q/motor1.reduction_ratio)
 
 
 
 params = A1_Params()
 motor1 = A1_Motor(serial_id=0, motor_id=0, 
-                  mode=params.ModeType.STOP)
+                  mode=params.ModeType.STOP, reduction_ratio=9.2)
 
-motor1.PosControl(5)
+time.sleep(0.5)
+
+motor1.SetParams(0.2, 1.0)
+
+motor1.AbsPosControl(0, 0)
+time.sleep(1)
+motor1.ReadData()
+print('position: ', motor1.data.q/motor1.reduction_ratio)
+
+motor1.IncPosControl(0, 0.2)
+time.sleep(1)
+motor1.ReadData()
+print('position: ', motor1.data.q/motor1.reduction_ratio)
+
+motor1.IncPosControl(0, -0.1)
+time.sleep(1)
+motor1.ReadData()
+print('position: ', motor1.data.q/motor1.reduction_ratio)
