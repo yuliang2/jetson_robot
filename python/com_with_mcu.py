5fe8468e2f937ebe7491339d04dbef5726168431
orinnx <orinnx@hkust-gz.edu.cn>
2025-01-23 12:56:00 +0800

    增加和MCU通信的CRC8校验

--- python/com_with_mcu.py
+++ python/com_with_mcu.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 # @File : com_with_mcu.py
 # @brief: jetson用于和mcu通信的程序
-# @note : 通信格式< 12A 34B 56C 78D 90E 12F 34G 56H >\r\n 其中A-H表示八个舵机
+# @note : 通信格式< 12A 34B 56C 78D 90E 12F 34G 56H CRC>\r\n 其中A-H表示八个舵机，CRC为2位0-F的ASCII码，注意H后有且仅有一个空格
 
-# TODO: 修改串口通信格式以便更高效稳定地和MCU传输数据
 # TODO: 修改command格式，以便于和仿真环境更好地对接
 
 from multiprocessing import Process, Queue
@@ -17,6 +16,18 @@ def log(msg):
     if log_q and log_q is not None:
         log_q.put(msg)
 
+def calculate_crc(data):
+    """计算CRC8校验值"""
+    crc = 0
+    for byte in data:
+        crc ^= byte
+        for _ in range(8):
+            if crc & 0x80:
+                crc = (crc << 1) ^ 0x07
+            else:
+                crc <<= 1
+        crc &= 0xFF
+    return crc
 
 class Com(Process):
 
@@ -27,19 +38,27 @@ class Com(Process):
         self.status_qs = status_qs
         self.command_q = command_q
         self.output = output
-
         self.log_q = log_q
 
     def command_decode(self, s):
-        pattern = r'<\s*(-?\d+)A\s*(-?\d+)B\s*(-?\d+)C\s*(-?\d+)D\s*(-?\d+)E\s*(-?\d+)F\s*(-?\d+)G\s*(-?\d+)H>'
+        pattern = r'<\s*(-?\d+)A\s*(-?\d+)B\s*(-?\d+)C\s*(-?\d+)D\s*(-?\d+)E\s*(-?\d+)F\s*(-?\d+)G\s*(-?\d+)H\s*([\da-fA-F]{2})\s*>'
         matches = re.findall(pattern, s)
         if matches == []:
             return None
-        values = matches[0]
+        values = matches[0][:-1]
+        crc_received = int(matches[0][-1], 16)
+
         keys = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
         results = {}
         for k, v in zip(keys, values):
             results[k] = int(v)
+
+        # 计算CRC以验证接收到的数据
+        crc_calculated = calculate_crc(s[:-3].encode())  # 去掉最后的CRC和'>'
+        if crc_calculated != crc_received:
+            log("[×] CRC校验失败")
+            return None
+
         return results
 
     def command_encode(self, command):
@@ -48,9 +67,9 @@ class Com(Process):
         for k, v in command.items():
             if k not in keys:
                 continue
-            else:
-                s += str(v) + k
-        return s + '>C\r\n'
+            s += str(v) + k
+        crc = calculate_crc(s.encode())
+        return f"{s}{crc:02X}>\r\n"
 
     def task(self):
         if self.port is None:
@@ -90,7 +109,8 @@ class Com(Process):
                 command = self.command_q.get()
                 s = self.command_encode(command).encode()
             else:
-                s = b"<>C\r\n"
+                s = b"<>00>\r\n"  # 发送空命令，CRC 设为 00
+
             if self.output:
                 log(s)
             ser.write(s)
@@ -109,6 +129,7 @@ class Com(Process):
             self.task()
             time.sleep(1)
 
+
 class PosPublish(Process):
     def __init__(self, command_q=None, output=False, log_q=None):
         Process.__init__(self)
@@ -121,9 +142,6 @@ class PosPublish(Process):
         log_q = self.log_q
         log("位置发布进程启动")
 
-        # self.command_q.put({'A': 2166, 'B': 1724, 'C': 1824, 'D': 1519, 'E': 2178, 'F': 1227, 'G': 430, 'H': 1973})
-        # time.sleep(1)
-
         while True:
             self.command_q.put({'A': 2266, 'B': 1724, 'C': 3800, 'D': 1519, 'E': 2178, 'F': 1227, 'G': 2369, 'H': 1973})
             # self.command_q.put({'A': 2266, 'B': 1824, 'C': 2024, 'D': 1619, 'E': 2278, 'F': 1327, 'G': 630, 'H': 2073})
@@ -135,10 +153,8 @@ class PosPublish(Process):
 if __name__ == "__main__":
     command_q = Queue(maxsize=10)
     command_q.put({'A': 2166, 'B': 1724, 'C': 3800, 'D': 1519, 'E': 2178, 'F': 1227, 'G': 2369, 'H': 1973})
-    com = Com(port="/dev/ttyUSB4", command_q=command_q, output=True)
-    # com.socket.rs485_mode = serial.rs485.RS485Settings()
+    com = Com(port="/dev/my485serial5", command_q=command_q, output=True)
     pos_publish = PosPublish(command_q=command_q, output=True)
-    # com.run()
     com.start()
     pos_publish.start()
     com.join()
